3b76c425b63694d8604204a7910cfe2570fd4de7
Irvy <Irving.Derin@gmail.com>
2012-12-29 15:29:04 -0500

    modified:   starterScript.js

--- starterScript.js
+++ starterScript.js
@@ -7,16 +7,48 @@
 var fs = require('fs'),
     spawn = require('child_process').spawn,
     http = require('http')
-    runningScripts= [];
+    runningScripts= [],
+    Statuses = {};
+
+
 
 // This is supposed to be a blocking operation. Don't start anything till this finishes!
 var scriptToBeRun = fs.readFileSync('scripts.txt').toString().split("\n");
-
-console.log(scriptToBeRun[1])
-
-numScripts = scriptToBeRun.length;
+var numScripts = scriptToBeRun.length;
 console.log(numScripts);
+console.log(scriptToBeRun)
 
+
+for(var i = 0; i < numScripts - 1 ; i++){
+    // minus 1 numScripts cause last element is empty
+    console.log("Spawning: " + scriptToBeRun[i]);
+    Statuses[scriptToBeRun[i]] = {} ;
+    Statuses[scriptToBeRun[i]]["Name"] = scriptToBeRun[i];
+   
+    runningScripts[i] = spawn('python', ['-u' , scriptToBeRun[i] ] );
+    
+    Statuses[scriptToBeRun[i]]["State"] = "Operational";
+
+    runningScripts[i].stdout.on('data', function(data) {
+        Statuses[scriptToBeRun[i]]["Data"] = data;
+    });
+
+    runningScripts[i].stdout.on('err', function(error) {
+        Statuses[scriptToBeRun[i]]["ErrData"] = error;
+        Statuses[scriptToBeRun[i]]["State"] = "Error";
+    });
+};
+
+setInterval(function(){
+    console.log(Statuses); 
+    console.log("-----------------------------------");
+    }, 5000);
+
+
+
+
+/*   I know this works, but I want to redo it
+ *   
 for(var i = 0; i < numScripts -1; i++){
     console.log(scriptToBeRun[i]);
     runningScripts[i] = spawn('python', ['-u' , scriptToBeRun[i]]);
@@ -28,8 +60,6 @@ for(var i = 0; i < numScripts -1; i++){
     runningScripts[i].stderr.on('data', function(err){
         console.log("There was an error: " + err)
     });
-
-
 }
-
+*/
 
