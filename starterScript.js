60f1cee011d4e984f6837439543d227509cb39ef
Irvy <Irving.Derin@gmail.com>
2013-01-03 01:11:09 -0500

    modified:   starterScript.js

--- starterScript.js
+++ starterScript.js
@@ -27,15 +27,15 @@ for(var i = 0; i < numScripts ; i++){
 
     runningScripts[i] = spawn('python', ['-u',scriptToBeRun[i] ]); //Creates the scripts
 
-    processName = scriptToBeRun[i];
+    //processName = scriptToBeRun[i];
 
-    Statuses[scriptToBeRun[i]] = {processName : {"Name" : scriptToBeRun[i], "PID" : runningScripts[i].pid}};
+    Statuses[scriptToBeRun[i]] =  {"Name" : scriptToBeRun[i], "PID" : runningScripts[i].pid};
 
 }
 
 for(var i = 0; i < runningScripts.length ; i++){
-        var index = i     
-        runningScripts[i].stdout.on("data", closureCallback(index, Statuses, scriptToBeRun))
+        runningScripts[i].stdout.on("data", stdOutCallback(i, Statuses, scriptToBeRun))  // Event handlers for each stream
+        runningScripts[i].stderr.on("data", stdErrCallback(i, Statuses, scriptToBeRun))
     };
 
 
@@ -44,14 +44,42 @@ setInterval(function(){
     console.log("-----------------------------------");
     }, 1000);
 
-function closureCallback(index, json_Stats, scriptsList){
+
+
+//////////////////////////////////// FUNCTIONS ///////////////////////////////////
+
+
+
+function stdOutCallback(index, json_Stats, scriptsList){
     return function(data){
-        console.log("Index: " + index);
-        console.log("Statuses: " + json_Stats);
-        console.log("Data: " + data);
-        console.log("Scripts: " + scriptsList[index]);
+        Statuses[scriptsList[index]]["Data"] = data.toString().replace('\n','');
+        Statuses[scriptsList[index]]["Status"] = "Operational" 
+
+    // Test to see how it actually works / what am I seeing
+       // console.log("Index: " + index);
+       // console.log("Statuses: " + json_Stats);
+       // console.log("Data: " + data);
+       // console.log("Scripts: " + scriptsList[index]);
         };
     }
+
+function stdErrCallback(index, json_Stats, scriptsList){
+    return function(error){  // All this is doing is populating the JSON that I am going to sending out. 
+                             // We're simply making a data packet. It might be a good idea to make this whle
+                             // thing into some module. That way what I'll get is a nice piece that I can
+                             // publish (I doubt people will really need something of this class
+                             //
+                             // Sending this to an interface might be soemething else. This makes
+                             // good for RO for example. And other things where you want to 
+                             // monitor several daemons. OH THAT'S THIS IS, A COMMUNICATOR 
+                             // WITH DAEMONS!!!! HOLY SHIT!!!
+        Statuses[scriptsList[index]]["Data"] = undefined;
+        Statuses[scriptsList[index]]["Error"] = error.toString();
+        Statuses[scriptsList[index]]["Status"] = "Error Occured";
+        };
+    }
+    
+
 
 
 
@@ -69,3 +97,4 @@ for(var i = 0; i < numScripts -1; i++){
         console.log("There was an error: " + err)
     });
 }
+*/
